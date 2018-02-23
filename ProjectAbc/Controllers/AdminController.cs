2ed2dad10137fe3376fbcc7e4429ddc52f12c5fe
Ashit Adhikari <aadhikari@kenall.com>
2018-02-23 14:14:07 -0600

    New app add. Format of the app is WebApi.

---
+++ ProjectAbc/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectAbc.Controllers
+{
+    [Route("")]
+    public class AdminController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
