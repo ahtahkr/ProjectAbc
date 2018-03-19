7d123be67d212789372be16636ec4fdf2b698310
Ashit Adhikari <as.adhikari@outlook.com>
2018-03-19 15:12:03 -0500

    ClearBit added to find the company.

---
+++ Classes/Company.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectAbc.Classes.Clearbit
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Domain { get; set; }
+        public string Logo { get; set; }
+    }
+}
