d114adc5a071e467516634e596b5797826ed6da0
Ashit Adhikari <as.adhikari@outlook.com>
2018-03-20 09:30:04 -0500

    IEXTrading class added to access the IEXTrading web api.

---
+++ IEXTrading/IEXTrading.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectAbc.IEXTrading
+{
+    public static class IEXTrading
+    {
+        private const string URI = "https://api.iextrading.com/1.0/";
+        public static string Chart(string symbol, string range = "" )
+        {
+            string full_uri = URI + "stock/"+symbol+"/chart/"+ range;
+            return Library.HttpRequestUtility.GetRequest(full_uri);
+        }
+    }
+}
