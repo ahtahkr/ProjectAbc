6af1fbf333e2813d9843011b0cea5eff5138cde5
Ashit Adhikari <as.adhikari@outlook.com>
2018-03-21 15:11:47 -0500

    NewsApi Key added to appsettings.json file.

--- Classes/News/NewsApiOrg.cs
+++ Classes/News/NewsApiOrg.cs
@@ -9,19 +9,19 @@ namespace ProjectAbc.Classes.News.NewsApiOrg
     {
         public const string URI = "https://newsapi.org/v2/";
 
-        public static string Top_Headlines(string Api_Key = "24763003c86c4c439e53ccd7014a1a80", string Country = "us")
+        public static string Top_Headlines(string Api_Key, string Country = "us")
         {
             string full_uri = URI + "top-headlines?country=" + Country + "&apiKey=" + Api_Key;
             return Library.HttpRequestUtility.GetRequest(full_uri);
         }
 
-        public static string Everything(string Api_Key = "24763003c86c4c439e53ccd7014a1a80", string KeyWord = "us")
+        public static string Everything(string Api_Key, string KeyWord = "us")
         {
-            string full_uri = URI + "everything?q=" + KeyWord + "&apiKey=" + Api_Key;
+            string full_uri = URI + "everything?q=" + KeyWord + "&sortBy=popularity&apiKey=" + Api_Key;
             return Library.HttpRequestUtility.GetRequest(full_uri);
         }
 
-        public static string Sources(string Api_Key = "24763003c86c4c439e53ccd7014a1a80")
+        public static string Sources(string Api_Key)
         {
             string full_uri = URI + "sources?apiKey=" + Api_Key;
             return Library.HttpRequestUtility.GetRequest(full_uri);
--- Controllers/NewsController.cs
+++ Controllers/NewsController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace ProjectAbc.Controllers
 {
@@ -15,9 +17,12 @@ namespace ProjectAbc.Controllers
         [HttpGet]
         public IActionResult Get()
         {
+            IConfigurationRoot configRoot = Helper.ConfigurationHelper.GetConfiguration(Directory.GetCurrentDirectory());
+            string Api_Key = configRoot.GetSection("environmentVariables")["News_Api_Key"];
+
             Classes.News.News News 
                 = JsonConvert.DeserializeObject<Classes.News.News>(
-                        Classes.News.NewsApiOrg.V2.Top_Headlines());
+                        Classes.News.NewsApiOrg.V2.Top_Headlines(Api_Key));
             News.Title = "Headlines";
             return View(News);
         }
@@ -30,9 +35,13 @@ namespace ProjectAbc.Controllers
             {
                 keyword = "America";
             }
+
+            IConfigurationRoot configRoot = Helper.ConfigurationHelper.GetConfiguration(Directory.GetCurrentDirectory());
+            string Api_Key = configRoot.GetSection("environmentVariables")["News_Api_Key"];
+
             Classes.News.News News
                 = JsonConvert.DeserializeObject<Classes.News.News>(
-                        Classes.News.NewsApiOrg.V2.Everything("24763003c86c4c439e53ccd7014a1a80", keyword));
+                        Classes.News.NewsApiOrg.V2.Everything(Api_Key, keyword));
             News.Title = keyword;
             return View(News);
         }
