8f7a3734783b5ae3a8f5d093a0534c32ad930fa0
Ashit Adhikari <as.adhikari@outlook.com>
2018-03-23 11:00:12 -0500

    Intrinio WebApi: ComnpanyNews HttpRequest added.

---
+++ Intrinio/WebApi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectAbc.Intrinio
+{
+    public static class WebApi
+    {
+        private const string URI = "https://api.intrinio.com/";
+        public static string CompanyNews(string identifier, string username, string password)
+        {
+            string full_uri = URI + "news?identifier=" + identifier;
+            return Library.HttpRequestUtility.GetRequest(full_uri, username, password);
+        }
+    }
+}
