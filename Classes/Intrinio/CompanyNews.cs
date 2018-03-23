a4c3da3c4eebff77e6dc492c60d32adcfd689559
Ashit Adhikari <as.adhikari@outlook.com>
2018-03-23 09:43:01 -0500

    Intrinio: Model added.

---
+++ Classes/Intrinio/CompanyNews.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectAbc.Classes.Intrinio
+{
+    public class CompanyNews
+    {
+        public List<Datum> Data { get; set; }
+        public int Result_Count { get; set; }
+        public int Page_Size { get; set; }
+        public int Current_Page { get; set; }
+        public int Total_Pages { get; set; }
+        public int Api_Call_Credits { get; set; }
+    }
+    public class Datum
+    {
+        public string Ticker { get; set; }
+        public string Figi_Ticker { get; set; }
+        public string Figi { get; set; }
+        public string Title { get; set; }
+        public DateTime Publication_Date { get; set; }
+        public string Summary { get; set; }
+        public string Url { get; set; }
+    }
+}
